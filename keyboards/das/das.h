2c7f86987a89d62177bbd72170bf4ef6c7ab9547
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2017-02-17 18:29:53 +0700

    Das keyboard

---
+++ keyboards/das/das.h
@@ -0,0 +1,12 @@
+#ifndef DAS_H
+#define DAS_H
+
+#include "quantum.h"
+
+// This a shortcut to help you visually see your layout.
+// The following is an example using the Planck MIT layout
+// The first section contains all of the arguements
+// The second converts the arguments into a two-dimensional array
+
+
+#endif
